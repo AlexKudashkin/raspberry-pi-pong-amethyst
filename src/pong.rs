9589dcb2eee181c7a073832d025f660f4c4d6db9
AlexKudashkin <jedi5kittens@gmail.com>
2019-04-06 18:40:11 -0700

    initial commit

---
+++ src/pong.rs
@@ -0,0 +1,66 @@
+extern crate amethyst;
+pub const ARENA_HEIGHT: f32 = 100.0;
+pub const ARENA_WIDTH: f32 = 100.0;
+pub const PADDLE_HEIGHT: f32 = 16.0;
+pub const PADDLE_WIDTH: f32 = 4.0;
+use amethyst::assets::{AssetStorage, Loader};
+use amethyst::core::transform::Transform;
+use amethyst::ecs::World;
+use amethyst::ecs::prelude::{Component, DenseVecStorage};
+use amethyst::prelude::*;
+use amethyst::renderer::{
+    Camera, PngFormat, Projection, SpriteRender, SpriteSheet,
+    SpriteSheetFormat, SpriteSheetHandle, Texture, TextureMetadata,
+};
+pub struct Pong;
+impl SimpleState for Pong { }
+
+struct MyState;
+impl SimpleState for MyState {
+    fn on_start(&mut self, data: StateData<'_, GameData<'_, '_>>) {
+        let world = data.world;
+
+        initialise_camera(world);
+    }
+}
+
+fn initialise_camera(world: &mut World) {
+    let mut transform = Transform::default();
+    transform.set_z(1.0);
+    world
+        .create_entity()
+        .with(Camera::from(Projection::orthographic(
+            0.0,
+            ARENA_WIDTH,
+            0.0,
+            ARENA_HEIGHT,
+        )))
+        .with(transform)
+        .build();
+}
+
+#[derive(PartialEq, Eq)]
+pub enum Side {
+    Left,
+    Right,
+}
+
+pub struct Paddle {
+    pub side: Side,
+    pub width: f32,
+    pub height: f32,
+}
+
+impl Component for Paddle {
+    type Storage = DenseVecStorage<Self>;
+}
+
+impl Paddle {
+    fn new(side: Side) -> Paddle {
+        Paddle {
+            side,
+            width: 1.0,
+            height: 1.0,
+        }
+    }
+}
