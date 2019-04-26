7b4eef12e3941b95723ec348a0928b3d31219f8b
AlexKudashkin <jedi5kittens@gmail.com>
2019-04-26 03:58:14 -0400

    Add font, add and update Scoreboard

--- src/main.rs
+++ src/main.rs
@@ -9,6 +9,7 @@ use amethyst::{
     renderer::{DisplayConfig, DrawFlat2D, Pipeline, RenderBundle, Stage},
     utils::application_root_dir,
     input::InputBundle,
+    ui::{DrawUi, UiBundle}
 };
 
 fn main() -> amethyst::Result<()> {
@@ -21,10 +22,10 @@ fn main() -> amethyst::Result<()> {
     let path = "./resources/display_config.ron";
     let config = DisplayConfig::load(&path);
 
-    let pipe = Pipeline::build().with_stage(
-        Stage::with_backbuffer()
-            .clear_target([0.0, 0.0, 0.0, 1.0], 1.0)
-            .with_pass(DrawFlat2D::new()),
+    let pipe = Pipeline::build().with_stage(Stage::with_backbuffer()
+                                                .clear_target([0.0, 0.0, 0.0, 1.0], 1.0)
+                                                .with_pass(DrawFlat2D::new())
+                                                .with_pass(DrawUi::new()), // <-- Add me
     );
 
     let binding_path = "./resources/bindings_config.ron";
@@ -36,6 +37,7 @@ fn main() -> amethyst::Result<()> {
         .with_bundle(RenderBundle::new(pipe, Some(config)).with_sprite_sheet_processor())?
         .with_bundle(TransformBundle::new())?
         .with_bundle(input_bundle)?
+        .with_bundle(UiBundle::<String, String>::new())?
         .with(systems::PaddleSystem, "paddle_system", &["input_system"])
         .with(systems::move_balls::MoveBallsSystem, "ball_system", &[])
         .with(
--- src/pong.rs
+++ src/pong.rs
@@ -3,12 +3,13 @@ extern crate amethyst;
 use amethyst::{
     assets::{AssetStorage, Loader},
     core::transform::Transform,
-    ecs::prelude::{Component, DenseVecStorage},
+    ecs::prelude::{Component, DenseVecStorage, Entity},
     prelude::*,
     renderer::{
         Camera, Flipped, PngFormat, Projection, SpriteRender, SpriteSheet, SpriteSheetFormat,
         SpriteSheetHandle, Texture, TextureMetadata,
     },
+    ui::{Anchor, TtfFormat, UiText, UiTransform},
 };
 
 pub const ARENA_HEIGHT: f32 = 100.0;
@@ -36,6 +37,7 @@ impl SimpleState for Pong {
         initialise_ball(world, sprite_sheet_handle.clone()); // <- add this line
         initialise_paddles(world, sprite_sheet_handle);
         initialise_camera(world);
+        initialise_scoreboard(world);
     }
 }
 
@@ -72,6 +74,19 @@ pub struct Ball {
 
 impl Component for Ball {
     type Storage = DenseVecStorage<Self>;
+}
+
+/// ScoreBoard contains the actual score data
+#[derive(Default)]
+pub struct ScoreBoard {
+    pub score_left: i32,
+    pub score_right: i32,
+}
+
+/// ScoreText contains the ui text components that display the score
+pub struct ScoreText {
+    pub p1_score: Entity,
+    pub p2_score: Entity,
 }
 
 fn load_sprite_sheet(world: &mut World) -> SpriteSheetHandle {
@@ -174,4 +189,45 @@ fn initialise_ball(world: &mut World, sprite_sheet_handle: SpriteSheetHandle) {
         })
         .with(local_transform)
         .build();
+}
+
+/// Initialises a ui scoreboard
+fn initialise_scoreboard(world: &mut World) {
+    let font = world.read_resource::<Loader>().load(
+        "font/square.ttf",
+        TtfFormat,
+        Default::default(),
+        (),
+        &world.read_resource(),
+    );
+    let p1_transform = UiTransform::new(
+        "P1".to_string(), Anchor::TopMiddle,
+        -50., -50., 1., 200., 50.,1
+    );
+    let p2_transform = UiTransform::new(
+        "P2".to_string(), Anchor::TopMiddle,
+        50., -50., 1., 200., 50.,1
+    );
+
+    let p1_score = world
+        .create_entity()
+        .with(p1_transform)
+        .with(UiText::new(
+            font.clone(),
+            "0".to_string(),
+            [1., 1., 1., 1.],
+            50.,
+        )).build();
+
+    let p2_score = world
+        .create_entity()
+        .with(p2_transform)
+        .with(UiText::new(
+            font.clone(),
+            "0".to_string(),
+            [1., 1., 1., 1.],
+            50.,
+        )).build();
+
+    world.add_resource(ScoreText { p1_score, p2_score });
 }
